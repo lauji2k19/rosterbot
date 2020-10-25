edc39308047fa2751e2a4a17b5c2400332681c85
Ji Lau <lauji2k12@gmail.com>
2020-10-25 16:15:42 -0500

    Change int values to int in constants

--- rosterbot/constants.py
+++ rosterbot/constants.py
@@ -1,10 +1,10 @@
 import os
 
 BOT_TOKEN = os.environ.get('ROSTER_BOT_TOKEN')
-DEVELOPER_USER_ID = os.environ.get('DEVELOPER_USER_ID')
+DEVELOPER_USER_ID = int(os.environ.get('DEVELOPER_USER_ID'))
 
-MASTER_GUILD_ID = os.environ.get('MASTER_GUILD_ID')
-MASTER_BOT_LOG_ID = os.environ.get('MASTER_BOT_LOG_ID')
+MASTER_GUILD_ID = int(os.environ.get('MASTER_GUILD_ID'))
+MASTER_BOT_LOG_ID = int(os.environ.get('MASTER_BOT_LOG_ID'))
 
 ROSTER_SPREADSHEET_ID = os.environ.get('ROSTERBOT_SPREADSHEET_ID')
 MARIA_DB_USER = os.environ.get('MARIA_DB_USER')
